30fd21f9995132947a8c919ad86ed5f4b406b343
helciodev <helcioandre@live.com>
2021-07-22 06:36:59 +0100

    Add lose game logic

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const inputNumber = document.querySelector('.guess');
 const numToGuess = Math.floor(Math.random() * 20);
 const btnCheck = document.querySelector('.check');
 const message = document.querySelector('.message');
+let initialScore = 20;
 
 console.log(numToGuess)
 btnCheck.addEventListener('click', () => {
@@ -14,11 +15,22 @@ btnCheck.addEventListener('click', () => {
   }else if(Number(inputNumber.value) === numToGuess){
     message.textContent = 'Correct 🙌🏽🎉🍾🎊';
   }else if(Number(inputNumber.value) < numToGuess){
-    message.textContent = 'Too low 👇🏽';
+    initialScore -= 1;
+   lose('Too low 👇🏽')
   }else if(Number(inputNumber.value) > numToGuess){
-    message.textContent = 'Too high 👆🏽';
+    initialScore -= 1;
+   lose('Too high 👆🏽')
   }
   console.log(Number(inputNumber.value))
 });
 
 
+function lose(write) {
+  if(initialScore > 1) {
+    score.textContent = initialScore;
+    message.textContent = write;
+  }else {
+    message.textContent = 'You lose the game 💩'
+    score.textContent = 0
+  }
+}
